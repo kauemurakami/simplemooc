4035f6a2bcc5cca986a5fb6d06ff1c46f0f39ed1
kauê murakami <kauetmurakami@gmail.com>
2019-05-09 16:03:26 -0300

    paginação, correção de exibição de questões do forum

--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.urls import path
-from simplemooc.courses import views
+from simplemooc.forum import views
 
 urlpatterns = [
 
--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from django.generic import TemplateView, View, ListView
+from django.views.generic import TemplateView, View, ListView
+
+from .models import Thread
 # Create your views here.
 
-class ForumView(TemplateView):
+#ListView herda de Template View logo todas as opções das duas classes estarao dispostas
+class ForumView(ListView):
 	
 	model = Thread
-	paginate_by = 10 #paginação de 10 em 10
+	paginate_by = 2 #paginação de 10 em 10
 	#template_name = '' 
+	template_name = 'forum/index.html'
+
+	# metodo que herda de ForumView, passando uma lista de parametros nomeados 
+	# chamamos a função novamente da classe pai (Forum View) retornando o contexto do template
+	def get_context_data(self, **kwargs):
+		context = super(ForumView,self).get_context_data(**kwargs)
+		context['tags'] = Thread.tags.all() # add /chama todas as tags associados a alguma instância dessa classe
+		return context
 
 	#obrigatorio passar o nome de um template, ou dentro da classe ou para o as_view()
 #variavel index vai receber o resultado do forumview as view e retorna uma função
-index = ForumView.as_view(template_name = 'forum/index.html')
+index = ForumView.as_view() #template name poderia vir aqui
 #as_view() retorna uma função semelhante a nossas funções padrões que retornam um http response para renderizar uma template
